63cc5c4b846cd0ac546c4dd40df2e12258a1032f
AleksaU <32110183+AleksaU@users.noreply.github.com>
2018-04-23 13:15:41 +0300

    project

---
+++ Frontend/src/Pizza_List.js
@@ -0,0 +1,147 @@
+/**
+ * Created by diana on 12.01.16.
+ */
+
+var pizza_info = [
+    {
+        id:1,
+        icon:'assets/images/man.jpg',
+        title: "Пацієнт 1",
+        type: 'М’ясна піца',
+        content: {
+            meat: ['ПІБ: Марченко Олена Василівна'],
+            chicken: ['Стать: Жіноча'],
+            cheese: ['Дата народження: 11.12.1991']
+           //pineapple: ['ананаси'],
+           // additional: ['томатна паста', 'петрушка']
+        },
+        small_size:{
+            weight: 370,
+            size: 30,
+            price: 99
+        },
+        big_size:{
+            weight: 660,
+            size: 40,
+            price: 169
+        },
+        is_new:true,
+        is_popular:true
+
+    },
+    {
+        id:2,
+        icon:'assets/images/man.jpg',
+        title: "Пацієнт 2",
+        type: 'М’ясна піца',
+        content: {
+            meat: ['мисливські ковбаски', 'ковбаски папероні', 'шинка'],
+            cheese: ['сир домашній'],
+            mushroom: ['шампінйони'],
+            additional: ['петрушка', 'оливки']
+        },
+        small_size:{
+            weight: 460,
+            size: 30,
+            price: 139
+        },
+        big_size:{
+            weight: 840,
+            size: 40,
+            price: 199
+        },
+        is_popular:true
+    },
+    {
+        id:3,
+        icon:'assets/images/man.jpg',
+        title: "Пацієнт 3",
+        type: 'М’ясна піца',
+        content: {
+            meat: ['вітчина', 'куриця копчена'],
+            cheese: ['сир моцарелла'],
+            pineapple: ['ананаси'],
+            additional: ['кукурудза', 'петрушка', 'соус томатний']
+        },
+        small_size:{
+            weight: 430,
+            size: 30,
+            price: 115
+        },
+        big_size:{
+            weight: 780,
+            size: 40,
+            price: 179
+        }
+    },
+    {
+        id:4,
+        icon:'assets/images/man.jpg',
+        title: "Пацієнт 4",
+        type: 'М’ясна піца',
+        content: {
+            meat: ['вітчина', 'салямі'],
+            cheese: ['сир моцарелла'],
+            mushroom: ['шампінйони'],
+            additional: ['перець болгарський',  'соус томатний']
+        },
+        small_size:{
+            weight: 450,
+            size: 30,
+            price: 111
+        },
+        big_size:{
+            weight: 790,
+            size: 40,
+            price: 169
+        }
+    },
+
+    {
+        id:43,
+        icon:'assets/images/man.jpg',
+        title: "Пацієнт 5",
+        type: 'М’ясна піца',
+        content: {
+            meat: ['ковбаски'],
+            cheese: ['сир моцарелла'],
+            mushroom: ['шампінйони'],
+            pineapple: ['ананаси'],
+            additional: ['цибуля кримська', 'огірки квашені', 'соус гірчичний']
+        },
+        small_size:{
+            weight: 470,
+            size: 30,
+            price: 115
+        },
+        big_size:{
+            weight: 780,
+            size: 40,
+            price: 180
+        }
+    },
+
+    {
+        id:6,
+        icon:'assets/images/man.jpg',
+        title: "Пацієнт 6",
+        type: 'Морська піца',
+        content: {
+            ocean: ['ікра червона', 'лосось копчений'],
+            cheese: ['сир моцарелла'],
+            additional: ['оливкова олія', 'вершки']
+        },
+        small_size:{
+            weight: 400,
+            size: 30,
+            price: 189
+        },
+        big_size:{
+            weight: 700,
+            size: 40,
+            price: 299
+        }
+    }
+];
+
+module.exports = pizza_info;
---
+++ Frontend/src/pizza/PizzaCart.js
@@ -0,0 +1,181 @@
+/**
+ * Created by chaika on 02.02.16.
+ */
+var Templates = require('../Templates');
+var Storage =window.localStorage;
+
+//Перелік розмірів піци
+var PizzaSize = {
+    Big: "big_size",
+    Small: "small_size"
+};
+
+//Змінна в якій зберігаються перелік піц в кошику
+var Cart = [];
+
+//HTML едемент куди будуть додаватися піци
+var $cart = $("#orderOne");
+
+var totalBuy = 0;
+var numberOfOrders = 0;
+
+function addToCart(pizza, size) {
+    //Додавання однієї піци в кошик покупок
+    //Приклад реалізації, можна робити будь-яким іншим способом
+    function samePizza(obj) {
+        return obj.pizza.id === pizza.id && obj.size === size;
+    }
+    var same = Cart.filter(samePizza);
+    if (same.length > 0) {
+        same[0].quantity++;
+    } else {
+        Cart.push({
+            pizza: pizza,
+            size: size,
+            quantity: 1
+        });
+        numberOfOrders++;
+        updateOrderNumber();
+    }
+    // Оновлюємо суму
+    totalBuy += pizza[size].price;
+    updateTotalSum();
+    updateCart();
+
+}
+
+function updateTotalSum(){
+    $("#totalBuy").html(totalBuy);
+    Storage.setItem("totalBuy",totalBuy);
+}
+
+function updateOrderNumber(){
+    $(".amount_of_orders").html(numberOfOrders);
+    Storage.setItem("numberOfOrders",numberOfOrders);
+}
+
+
+function removeFromCart(cart_item) {
+    //Видалити піцу з кошика
+    Cart.splice(Cart.indexOf(cart_item), 1);
+    //Після видалення оновити відображення
+    updateCart();
+}
+
+function initialiseCart() {
+    //Фукнція віпрацьвуватиме при завантаженні сторінки
+    //Тут можна наприклад, зчитати вміст корзини який збережено в Local Storage то показати його
+    var saved = Storage.getItem("cart");
+    if (saved) {
+        Cart = saved;
+        totalBuy = Storage.getItem("total");
+        updateTotalSum();
+        numberOfOrders = Storage.getItem("numberOfOrders");
+        updateOrderNumber();
+    }
+    updateCart();
+
+}
+
+function getPizzaInCart() {
+    //Повертає піци які зберігаються в кошику
+    return Cart;
+}
+
+function updateCart() {
+    //Функція викликається при зміні вмісту кошика
+    //Тут можна наприклад показати оновлений кошик на екрані та зберегти вміт кошика в Local Storage
+
+    //Очищаємо старі піци в кошику
+    $cart.html("");
+
+    //Онволення однієї піци
+    function showOnePizzaInCart(cart_item) {
+        var html_code = Templates.PizzaCart_OneItem(cart_item);
+
+        var $node = $(html_code);
+
+        $node.find(".plus").click(function () {
+            //Збільшуємо кількість замовлених піц
+            cart_item.quantity += 1;
+
+            totalBuy += cart_item.pizza[cart_item.size].price;
+
+            //Оновлюємо відображення
+            updateCart();
+            updateTotalSum();
+
+        });
+
+        $node.find(".minus").click(function () {
+            //Зменшуємо
+            if (cart_item.quantity == 1) {
+                removeFromCart(cart_item);
+                numberOfOrders--;
+
+                updateOrderNumber();
+            }
+            cart_item.quantity -= 1;
+            totalBuy -= cart_item.pizza[cart_item.size].price;
+            //Оновлюємо відображення
+            updateCart();
+            updateTotalSum();
+        });
+        $node.find(".delete").click(function () {
+            removeFromCart(cart_item);
+            totalBuy -= cart_item.pizza[cart_item.size].price * cart_item.quantity;
+            numberOfOrders--;
+            updateOrderNumber();
+            updateTotalSum();
+            //Оновлюємо відображення
+            updateCart();
+            updateTotal();
+        });
+
+        $cart.append($node);
+
+    }
+
+    Cart.forEach(showOnePizzaInCart);
+    Storage.setItem("cart", Cart);
+    if (Cart.length === 0) {
+        $("#orderButt").attr("disabled", "disabled");
+    } else {
+        $("#orderButt").removeAttr("disabled");
+    }
+}
+    $("#clear").click(function () {
+        clear();
+    });
+
+    function clear(){
+        Cart = [];
+        updateCart();
+        totalBuy= 0;
+        updateTotalSum();
+        numberOfOrders = 0;
+        updateOrderNumber();
+    }
+
+
+    $("#orderButt").click(function () {
+        if(Cart.length !== 0){
+            location.href="/order.html";
+        }
+    });
+
+    $("#editButt").click(function () {
+        if(Cart.length !== 0){
+            location.href="/";
+        }
+    });
+
+exports.removeFromCart = removeFromCart;
+exports.addToCart = addToCart;
+
+exports.getPizzaInCart = getPizzaInCart;
+exports.initialiseCart = initialiseCart;
+
+exports.PizzaSize = PizzaSize;
+exports.getPizzaInCart = getPizzaInCart;
+//exports.clear = clear;
